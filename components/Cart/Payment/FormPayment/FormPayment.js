048f3722be7985c2f8db29e5fc8715efba995435
enriqueamate <enriqueaamate@gmail.com>
2022-05-18 20:34:31 -0500

    Forma de Pago creada
    Se creó la forma de pago y el formulario para insertar la tarjeta, conectado a stripe

---
+++ components/Cart/Payment/FormPayment/FormPayment.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react'
+import { Button} from "semantic-ui-css";
+import { useRouter } from "next/router";
+import { CardElement, useStripe, useElements, CardElement} from "@stripe/react-stripe-js";
+import { toast } from "@react-toastify";
+import { size } from "lodash";
+import useAuth from "../../../../hooks/useAuth";
+import useCart from "../../../../hooks/useCart";
+
+export default function FormPayment(props) {
+    const {products, address} = props;
+    const [loading, setLoading] = useState(false)
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const handleSubmit = async (event) =>{
+        event.preventDefault();
+        setLoading(true);
+
+        if(!stripe || !elements) return;
+
+        const CardElement = elements.getElement(CardElement);
+        const result =await stripe.createToken(CardElement);
+
+        if(result.error){
+            toast.error(result.error.message);
+
+        } else{
+            console.log(result);
+        }
+        
+        setLoading(false);
+    }
+
+  return (
+    <form className='form-payment' onSubmit={handleSubmit}>
+        <CardElement />
+        <Button type="submit" loading={loading} disabled={!stripe}>Pagar</Button>
+    </form>
+  );
+}
--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,4 @@
 export const BASE_PATH = "http://localhost:1337";
 export const TOKEN = "token"
 export const CART = "cart"
+export const STRIPE_TOKEN = "pk_test_51Kz3zELYCIUZ7yCtkTXbIWkCFR1LfGYcTy9QrQTkFNa4TEx0qzLiM8kcBRcOkY7HaqsIZIxiKIBpuiuoAcE12kz400P3g4nxwW";
