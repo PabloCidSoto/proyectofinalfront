1a308850177b0712ba1bc457c18ba0481c4a7bb7
Pablo Cid <pablocidsoto@gmail.com>
2022-05-22 15:54:02 -0500

    agregado la const de prod

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_PATH = "http://localhost:1337";
+export const BASE_PATH = "https://frikilince.herokuapp.com";
 export const TOKEN = "token"
 export const CART = "cart"
 export const STRIPE_TOKEN = "pk_test_51L1AJEAWIbFxbjlCyN4R3nyjxKOjci9NjMHmKSNH7TxurSGvIv7QsHO9tONUZkFPuOiofAtzW3Mj9dePYzQSFUCl00CWhEEsJM"
