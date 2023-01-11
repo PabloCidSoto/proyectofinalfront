3e096b7f0ce5d4aa31c5ac59bcd3ebec4ebabdf1
Pablo Emilio Cid Soto <pablocidsoto@gmail.com>
2023-01-10 20:41:04 -0600

    Update constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_PATH = "https://frikilince.herokuapp.com";
+export const BASE_PATH = "https://proyectofinaltapw.herokuapp.com";
 export const TOKEN = "token"
 export const CART = "cart"
 export const STRIPE_TOKEN = "pk_test_51L1AJEAWIbFxbjlCyN4R3nyjxKOjci9NjMHmKSNH7TxurSGvIv7QsHO9tONUZkFPuOiofAtzW3Mj9dePYzQSFUCl00CWhEEsJM"
